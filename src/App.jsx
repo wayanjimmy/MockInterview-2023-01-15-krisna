f6bf8965a0ed3335bbac72d23c4c6eb9783380c9
Wayan jimmy <wayanjimmy@users.noreply.github.com>
2023-01-15 12:17:06 +0000

    update gist url

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import './App.css'
 
-const url = "https://gist.githubusercontent.com/wayanjimmy/a79dba0ef6a47d2a3d113e6400bb60ba/raw/25428e96611bae8fc6fcc0486561d0b0d2c0ad36/cat_breeds.json"
+const url = "https://gist.githubusercontent.com/wayanjimmy/a79dba0ef6a47d2a3d113e6400bb60ba/raw/98dd43716e9490b24313c8de8e821238550479bc/cat_breeds.json"
 
 export default function App() {
   return (
