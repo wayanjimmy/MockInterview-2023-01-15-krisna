e5e98a07c7895d3e41ff105326c8d12a746d897d
Wayan jimmy <wayanjimmy@users.noreply.github.com>
2023-01-15 12:13:39 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react'
+import './App.css'
+
+const url = "https://gist.githubusercontent.com/wayanjimmy/a79dba0ef6a47d2a3d113e6400bb60ba/raw/25428e96611bae8fc6fcc0486561d0b0d2c0ad36/cat_breeds.json"
+
+export default function App() {
+  return (
+    <main>
+      Hello World
+    </main>
+  )
+}
